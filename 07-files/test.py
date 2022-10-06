7c80f6467a6d5594aeb25f626811c273ae92e436
everr <everrwsr@outlook.com>
2022-10-06 11:33:00 +0800

    add 08

--- 07-files/test.py
+++ 07-files/test.py
@@ -1,3 +1,6 @@
-fhand = open("test.txt", "r")
-line = fhand.readline()
-print(line) 
+# fhand = open("test.txt", "r")
+# line = fhand.readline()
+# print(line) 
+stuff = 'Hello\nworld'
+print(stuff)
+len(stuff)
